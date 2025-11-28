df90d79b746f93ee8b5c9a26a57417e0bc916816
kdh <dosl196122@naver.com>
2025-11-28 17:32:08 +0900

    Feat: 게시글 좋아요 기능 구현

--- core/views/board.py
+++ core/views/board.py
@@ -4,6 +4,9 @@ from ..serializers.board import BoardSerializer
 from ..models.board import Board
 from ..services.board import BoardService
 
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+
 class BoardViewSet(viewsets.ModelViewSet):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
@@ -11,3 +14,12 @@ class BoardViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         category_param = self.request.query_params.get('category')
         return BoardService.get_board_list(category_param)
+    
+    @action(detail=True, methods=['POST'], permission_classes = [IsAuthenticated], url_path='like')
+    def like(self, request, pk=None):
+        is_created = BoardService.toggle_like(request.user, pk)
+        
+        if is_created:
+            return Response(status=status.HTTP_201_CREATED)
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
