1b93aca95ef92c8d876d633bf0fc561e0b3be8b6
chlendyd7 <chlendyd7@naver.com>
2025-12-16 22:10:55 +0900

    feat: celery redis 추가 환경설정

--- config/settings.py
+++ config/settings.py
@@ -214,3 +214,8 @@ LOGGING = {
         },
     },
 }
+
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+# CELERY_ACCEPT_CONTENT = ['json']
+# CELERY_TASK_SERIALIZER = 'json'
