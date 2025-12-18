859720e94fb1ae146265f8b8fd44d6f244d1d73f
chlendyd7 <chlendyd7@naver.com>
2025-12-18 22:24:38 +0900

    feat: admin은 사용하지 않음

--- core/admin/__init__.py
+++ core/admin/__init__.py
@@ -1,8 +1,2 @@
 from django.contrib import admin
 from core.models import ChatSession
-
-@admin.register(ChatSession)
-class ChatSessionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'title', 'created_at', 'updated_at')
-    list_filter = ('created_at', 'updated_at')
-    search_fields = ('title', 'user__email')
