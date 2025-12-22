52c9a068664ee18b97953d2b1316bfed1079b1c5
chlendyd7 <chlendyd7@naver.com>
2025-12-22 16:56:50 +0900

    feat: username을 꼭 받지 않아도 됨

--- core/models/user.py
+++ core/models/user.py
@@ -1,7 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        null=True,
+        blank=True
+    )
     profile_image = models.CharField(
         'profile image URL',
         max_length=500,
@@ -13,7 +40,8 @@ class User(AbstractUser):
         max_length=255,
         unique=True,
     )
-    
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -24,4 +52,4 @@ class User(AbstractUser):
         verbose_name_plural = 'users'
 
     def __str__(self):
-        return self.username
+        return self.username or self.email
